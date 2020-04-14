b012f4fb409d0de4b4a5297b8dc78e1d99691357
sakaokimiaki <sakaokimiaki33@yahoo.co.jp>
2020-04-14 20:34:00 +0900

    prauto

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,35 @@
 $(function(){
+  var reloadMessages = function(){
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages){
+      if (messages.length !== 0){
+      var insertHTML = '';
+      $.each(messages, function(i, message){
+        insertHTML += buildHTML(message)
+      });
+      $('.messages').append(insertHTML);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function(){
+      alert('error');
+    });
+  };
+  setInterval(reloadMessages, 7000);
+  if (document.location.href.match(/\/groups\/\d+\/messages/)){
+  }
+
+  
   function buildHTML(message){
     if ( message.image ) {
       var html = 
-        `<div class="message">
+        `<div class="message" data-message-id=${message.id}>
           <div class="message__upper-info">
             <div class="message__upper-info__talker">
               ${message.user_name}
@@ -19,7 +46,7 @@ $(function(){
       return html;
      } else {
       var html = 
-        `<div class="message">
+        `<div class="message" data-message-id=${message.id}>
           <div class="message__upper-info">
             <div class="message__upper-info__talker">
               ${message.user_name}
